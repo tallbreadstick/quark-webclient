1a25869eedf7631487c15c2d2ef6677de3d9b1b3
Kenn Xavier <kennxavier15@gmail.com>
2025-11-25 18:46:56 +0800

    fix: changed learner value to STUDENT to match backend user type

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -79,8 +79,8 @@ export default function RegisterPage() {
                                         className="w-full px-4 py-2 bg-white/5 border border-white/10 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500/50 appearance-none pr-10 transition [&>option]:bg-slate-900 [&>option]:text-white [&>option:checked]:bg-blue-600 cursor-pointer"
                                     >
                                         <option value="" disabled className="text-gray-400">Select User Type</option>
-                                        <option value="learner">Learner</option>
-                                        <option value="educator">Educator</option>
+                                        <option value="STUDENT">Learner</option>
+                                        <option value="EDUCATOR">Educator</option>
                                     </select>
                                     <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
                                         <svg className="w-4 h-4 text-gray-400" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
